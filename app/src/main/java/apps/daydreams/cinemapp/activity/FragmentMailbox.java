9c1c350e2792a7c0b0ff56fa43c52cff1572d87d
Jumper <jomartinezch@gmail.com>
2014-11-10 00:12:07 -0500

    beta about us and contact us

--- app/src/main/java/apps/daydreams/cinemapp/activity/FragmentMailbox.java
+++ app/src/main/java/apps/daydreams/cinemapp/activity/FragmentMailbox.java
@@ -2,9 +2,12 @@ package apps.daydreams.cinemapp.activity;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import apps.daydreams.cinemapp.R;
 
@@ -13,11 +16,26 @@ import apps.daydreams.cinemapp.R;
  */
 public class FragmentMailbox extends Fragment {
 
+    private Button send;
+    private EditText mailText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_mailbox, container, false);
+
+        send = (Button) rootView.findViewById(R.id.send);
+        mailText = (EditText) rootView.findViewById(R.id.mailbox_edit_text);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("text->>>>>>>>>>>>>>>>", mailText.getText().toString());
+                mailText.setText("");
+                mailText.clearFocus();
+            }
+        });
         return rootView;
     }
+
 }
